f683b6992f55be6ca8948fa3cc351b2e5ca32c16
lukkshh <xlukkass@gmail.com>
2025-05-23 20:26:14 +0400

    add database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { sequelize } from "./config/database";
 
 const app = express();
 
@@ -7,3 +8,15 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+sequelize
+  .sync({ alter: false })
+  .then(() => {
+    console.log("Database synced!");
+    app.listen(3000, () => {
+      console.log("Server started on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Unable to sync database:", err);
+  });
